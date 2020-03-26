96b117704897993f22824568373dc15e5824a348
fer <ferconde87@gmail.com>
2020-03-26 16:43:31 +0700

    Updates

---
+++ precache-manifest.e95abc0460d6a8d6edeb8a2b7fb5d303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6512f7698178b108953bf7cc284cd2d2",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "e530d97e6008f9a69f51",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "a7a743f42dfe253da73c",
+    "url": "/tic-tac-toe/static/js/2.9314f0b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.9314f0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e530d97e6008f9a69f51",
+    "url": "/tic-tac-toe/static/js/main.f8bd6613.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
