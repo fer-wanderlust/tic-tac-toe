1728b18d7a643a4d564e5d5bf13c666c64edfb4e
fer <ferconde87@gmail.com>
2020-03-26 16:57:49 +0700

    Draw!

--- src/index.js
+++ src/index.js
@@ -41,9 +41,12 @@ class Board extends React.Component {
     }
   
     render() {
-      const winner = calculateWinner(this.state.squares);
       let status;
-      if (winner) {
+      const draw = isDraw(this.state.squares);
+      const winner = calculateWinner(this.state.squares);
+      if(draw){
+        status = 'Draw'
+      } else if (winner) {
         status = 'Winner: ' + winner;
       } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
@@ -88,6 +91,11 @@ class Board extends React.Component {
     }
   }
 
+  function isDraw(squares){
+    const isNotNull = (value) => value != null;
+    return squares.every(isNotNull);
+  }
+
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
@@ -99,12 +107,14 @@ class Board extends React.Component {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
     }
+
     return null;
   }
   
